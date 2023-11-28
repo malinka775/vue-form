8f92a4c1bc265d0941b407cbe2131f25e11c5d18
Alina <malinka775@yandex.ru>
2023-11-28 23:07:24 +0200

    refactor: delete unused var

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import RegistrationView from '../views/RegistrationView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
